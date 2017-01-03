b17525864f8d82f5db7b85ef2b39427e18102369
Beylul <bkbreab8@gmail.com>
2017-01-03 14:06:27 -0800

    add key into wrapper.

--- lib/chi_api_wrapper.rb
+++ lib/chi_api_wrapper.rb
@@ -1,5 +1,7 @@
 require 'httparty'
 
-Class ChiApiWrapper
-
+class ChiApiWrapper
+BASE_URL = "https://data.cityofchicago.org/resource/62iq-5k5u.json/"
+App_Token = ENV["App_Token"]
+Secret_Token = ENV["Secret_Token"]
 end
